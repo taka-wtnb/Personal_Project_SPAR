9285934897dbfc455f717c5b0356076cd17d35da
taka-wtnb <wa040280@gmail.com>
2020-09-02 19:12:26 -0700

    updated source files

--- src/pages/onTimeDelivery/OnTimeDelivery.js
+++ src/pages/onTimeDelivery/OnTimeDelivery.js
@@ -11,7 +11,7 @@ import OTDPieChart from '../../components/Charts/OTDPieChart/OTDPieChart';
 import OTDBestTable from '../../components/Tables/OTDTables/OTDBestTable';
 import OTDWorstTable from '../../components/Tables/OTDTables/OTDWorstTable';
 
-import s from './OTD.module.scss';
+import s from './OnTimeDelivery.module.scss';
 
 class OTD extends React.Component {
   render() {
