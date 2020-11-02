4d1c165fc1eeb2549f4f1011b38397c1e1cd5e80
taka-wtnb <wa040280@gmail.com>
2020-11-01 18:55:12 -0800

    changed the chart color

--- src/components/Charts/QualityManagementBarChart/chartOptions.js
+++ src/components/Charts/QualityManagementBarChart/chartOptions.js
@@ -27,7 +27,7 @@ export default function chartOptions(data) {
                 }
             }
         },
-        colors: ['#f0af03'],
+        colors: ['#F5F5DC'],
         plotOptions: {
             bar: {
             columnWidth: '45%',
